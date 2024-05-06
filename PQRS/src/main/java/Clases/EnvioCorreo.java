a0fa517fba473dab2b08e74c68423fda06f8fd75
johan1101 <johanrealpelibro@gmail.com>
2024-05-06 13:06:36 -0500

    Enviar correo version demo

---
+++ PQRS/src/main/java/Clases/EnvioCorreo.java
@@ -0,0 +1,77 @@
+package Clases;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EnvioCorreo {
+
+    private static String emailFrom = "";
+    private static String passwordFrom = "";
+    private String emailTo;
+    private String subject;
+    private String content;
+
+    private Properties mProperties;
+    private Session mSession;
+    private MimeMessage mCorreo;
+
+    public EnvioCorreo() {
+        mProperties = new Properties();
+    }
+
+    public void setEmailFrom(String emailFrom) {
+        this.emailFrom = emailFrom;
+    }
+
+    public void setPasswordFrom(String passwordFrom) {
+        this.passwordFrom = passwordFrom;
+    }
+
+    public void setTo(String emailTo) {
+        this.emailTo = emailTo;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void sendEmail() throws MessagingException {
+        createEmail();
+
+        Transport mTransport = mSession.getTransport("smtp");
+        mTransport.connect(emailFrom, passwordFrom);
+        mTransport.sendMessage(mCorreo, mCorreo.getRecipients(Message.RecipientType.TO));
+        mTransport.close();
+    }
+
+    private void createEmail() throws MessagingException {
+        // Configurar propiedades del servidor SMTP
+        mProperties.put("mail.smtp.host", "smtp.gmail.com");
+        mProperties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        mProperties.setProperty("mail.smtp.starttls.enable", "true");
+        mProperties.setProperty("mail.smtp.port", "587");
+        mProperties.setProperty("mail.smtp.user", emailFrom);
+        mProperties.setProperty("mail.smtp.ssl.protocols", "TLSv1.2");
+        mProperties.setProperty("mail.smtp.auth", "true");
+
+        // Obtener la sesión
+        mSession = Session.getDefaultInstance(mProperties);
+
+        // Crear el mensaje de correo
+        mCorreo = new MimeMessage(mSession);
+        mCorreo.setFrom(new InternetAddress(emailFrom));
+        mCorreo.setRecipient(Message.RecipientType.TO, new InternetAddress(emailTo));
+        mCorreo.setSubject(subject);
+        mCorreo.setText(content, "ISO-8859-1", "html");
+    }
+}
+
--- PQRS/src/main/java/Servlets/SvResponderSolicitud.java
+++ PQRS/src/main/java/Servlets/SvResponderSolicitud.java
@@ -4,8 +4,9 @@
  */
 package Servlets;
 
+import Clases.EnvioCorreo;
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.mail.MessagingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -35,6 +36,27 @@ public class SvResponderSolicitud extends HttpServlet {
             throws ServletException, IOException {
         processRequest(request, response);
         System.out.println("Corriendo metodo para responder solicitud");
+        String emailFrom = "pqrsresponse@gmail.com"; // Cambia esto por tu dirección de correo electrónico
+        String passwordFrom = "wkro wbtv mnoi gsjy"; // Cambia esto por tu contraseña
+        String emailTo = "johanrealpelibro@gmail.com"; // Cambia esto por la dirección de correo electrónico del destinatario
+        String subject = "Asunto del correo";
+        String content = "Contenido del correo electrónico.";
+
+        // Crear instancia de EnvioCorreos y configurar detalles del correo
+        EnvioCorreo envioCorreos = new EnvioCorreo();
+        envioCorreos.setEmailFrom(emailFrom);
+        envioCorreos.setPasswordFrom(passwordFrom);
+        envioCorreos.setTo(emailTo);
+        envioCorreos.setSubject(subject);
+        envioCorreos.setContent(content);
+
+        // Intentar enviar el correo electrónico
+        try {
+            envioCorreos.sendEmail();
+            response.getWriter().println("Correo enviado exitosamente");
+        } catch (MessagingException ex) {
+            response.getWriter().println("Error al enviar el correo: " + ex.getMessage());
+        }
     }
 
 
